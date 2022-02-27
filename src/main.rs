58b5ec6aad2604404f0131469eeb6bc5c881d22f
Moreal <dev.moreal@gmail.com>
2022-02-28 04:46:28 +0900

    Follow appearance

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,37 @@ use yew::prelude::*;
 #[function_component(App)]
 fn app() -> Html {
     html! {
-        <h1>{ "Hello World" }</h1>
+        <section>
+            <div>
+                <header>
+                    <h1>{ "todos" }</h1>
+                    <input value={""}/>
+                </header>
+                <section>
+                    <ul>
+                        <li>
+                            <input type="checkbox"/>
+                            <label>{ "Hello" }</label>
+                            <button></button>
+                        </li>
+                        <li>
+                            <input type="checkbox"/>
+                            <label>{ "AAA" }</label>
+                            <button></button>
+                        </li>
+                    </ul>
+                </section>
+                <footer>
+                    <span class="todo-count">
+                        <strong>{ 2 }</strong>
+                        <span>{" items left"}</span>
+                    </span>
+                    <ul><li><a>{ "All" }</a></li></ul> 
+                    <ul><li><a href="#/active">{ "Active" }</a></li></ul> 
+                    <ul><li><a href="#/completed">{ "Completed" }</a></li></ul>
+                </footer>
+            </div>
+        </section>
     }
 }
 

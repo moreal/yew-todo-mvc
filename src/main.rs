68e53ab3f3c28ad74ccdd9a9e41e9984a12368ea
Moreal <dev.moreal@gmail.com>
2022-02-28 04:55:41 +0900

    Make `App` functional component to component

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,50 @@
 use yew::prelude::*;
 
-#[function_component(App)]
-fn app() -> Html {
-    html! {
-        <section>
-            <div>
-                <header>
-                    <h1>{ "todos" }</h1>
-                    <input value={""}/>
-                </header>
-                <section>
-                    <ul>
-                        <li>
-                            <input type="checkbox"/>
-                            <label>{ "Hello" }</label>
-                            <button></button>
-                        </li>
-                        <li>
-                            <input type="checkbox"/>
-                            <label>{ "AAA" }</label>
-                            <button></button>
-                        </li>
-                    </ul>
-                </section>
-                <footer>
-                    <span class="todo-count">
-                        <strong>{ 2 }</strong>
-                        <span>{" items left"}</span>
-                    </span>
-                    <ul><li><a>{ "All" }</a></li></ul> 
-                    <ul><li><a href="#/active">{ "Active" }</a></li></ul> 
-                    <ul><li><a href="#/completed">{ "Completed" }</a></li></ul>
-                </footer>
-            </div>
-        </section>
+struct App {}
+
+impl Component for App {
+    type Message = ();
+
+    type Properties = ();
+
+    fn create(_: &Context<Self>) -> Self {
+        App {}
+    }
+
+    fn view(&self, _: &Context<Self>) -> Html {
+        html! {
+            <section>
+                <div>
+                    <header>
+                        <h1>{ "todos" }</h1>
+                        <input value={""}/>
+                    </header>
+                    <section>
+                        <ul>
+                            <li>
+                                <input type="checkbox"/>
+                                <label>{ "Hello" }</label>
+                                <button></button>
+                            </li>
+                            <li>
+                                <input type="checkbox"/>
+                                <label>{ "AAA" }</label>
+                                <button></button>
+                            </li>
+                        </ul>
+                    </section>
+                    <footer>
+                        <span class="todo-count">
+                            <strong>{ 2 }</strong>
+                            <span>{" items left"}</span>
+                        </span>
+                        <ul><li><a>{ "All" }</a></li></ul> 
+                        <ul><li><a href="#/active">{ "Active" }</a></li></ul> 
+                        <ul><li><a href="#/completed">{ "Completed" }</a></li></ul>
+                    </footer>
+                </div>
+            </section>
+        }
     }
 }
 

6c71bf446f05b09608c41d744517a2dc3f8cc2af
moreal <dev.moreal@gmail.com>
2022-02-28 12:20:28 +0000

    Deploying to gh-pages from @ moreal/yew-todo-mvc@b0f7b096c7c89174004aae58d5c3a447c04611d7 🚀

--- index-5406625e1c23eebf.js
+++ index-5406625e1c23eebf.js
@@ -270,7 +270,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('index-aa99cc75bc26d034_bg.wasm', import.meta.url);
+        input = new URL('index-5406625e1c23eebf_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports.wbg = {};
@@ -519,7 +519,7 @@ async function init(input) {
     imports.wbg.__wbindgen_throw = function(arg0, arg1) {
         throw new Error(getStringFromWasm0(arg0, arg1));
     };
-    imports.wbg.__wbindgen_closure_wrapper2813 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper2814 = function(arg0, arg1, arg2) {
         var ret = makeClosure(arg0, arg1, 110, __wbg_adapter_20);
         return addHeapObject(ret);
     };
